c7641579b8f55d9a188ae527f778b1fcfa0b5d99
helia57 <132613751+helia57@users.noreply.github.com>
2023-08-29 11:54:53 +0200

    autres versions boutondelete

--- FrontEnd/assets/script/app.js
+++ FrontEnd/assets/script/app.js
@@ -1,6 +1,6 @@
 
  // Récupération de l'élément du DOM qui accueillera les projets: <section id = "container-gallery-modale">
- let baliseDivContainerGalleryModale = document.querySelector("#container-gallery-modale");
+const baliseDivContainerGalleryModale = document.querySelector("#container-gallery-modale");
  // affichage de la modale 1
 const modalContainer = document.querySelector(".modal-container");
 const modalTriggers = document.querySelectorAll(".modal-trigger");
@@ -17,7 +17,9 @@ btnValiderModal2.classList.remove("active");
 // bouton pour fermer la modale2
 const btnFermerModale2= document.querySelector("#trigger2");
 const fermerModale2Overlay = document.querySelector(".overlay2");
-  
+const token = sessionStorage.accessToken;
+console.log(token)
+
 
 /************************************ */
 // fonction generer galerie modal     */
@@ -26,62 +28,58 @@ modalTriggers.forEach(trigger => trigger.addEventListener("click", toggleModal))
 function toggleModal(e) {
   
   modalContainer.classList.toggle("active");
+  
   baliseDivContainerGalleryModale.innerHTML ="";
   for (let i = 0; i < works.length; i++) {
     
-    const projet = works[i] 
+    const work = works[i] 
    
       //fiche projet dans une figure
   let baliseFigureModale = document.createElement("figure");
+  baliseFigureModale.dataset.id = work.id;
   baliseFigureModale.classList.add("works-projet-modale");
   baliseFigureModale.classList = "projets-modale";
   baliseDivContainerGalleryModale.appendChild(baliseFigureModale); 
           
       // une image
   let imageElementModale = document.createElement("img");
-  imageElementModale.src = projet.imageUrl;
+  imageElementModale.src = work.imageUrl;
   imageElementModale.setAttribute = "alt", "image du rendu du projet ${works.title}";
   imageElementModale.classList = "image-modale";
-  imageElementModale.innerText = projet.imageUrl ?? "image bientôt disponible";  // si pas d'image, return "image bientôt disponible"
+  imageElementModale.innerText = work.imageUrl ?? "image bientôt disponible";  // si pas d'image, return "image bientôt disponible"
   baliseFigureModale.append(imageElementModale);
 
-      //une div "wrapper-container-icons"  
-  const divIconsContainer = document.createElement("div");
-  divIconsContainer.classList.add("wrapper-container-icons");
-  baliseFigureModale.appendChild(divIconsContainer);
     // une div qui envellope icon "up-down"
-  const divIconCross = document.createElement("div");
-  divIconCross.classList.add("container-icons");
-  divIconCross.classList.add("container-cross");
-  divIconsContainer.appendChild(divIconCross);
+  const divIconCross = document.createElement("span");
+  divIconCross.classList.add("container-cross"); 
+  baliseFigureModale.appendChild(divIconCross);
     //et l'icon " up down"
   const iconCross = document.createElement("i");
   iconCross.classList.add("fa-solid");
-  iconCross.classList.add("fa-xs");
+  
   iconCross.classList.add("fa-up-down-left-right");
   divIconCross.appendChild(iconCross);
-  // une div qui envellope icon "trash"(poubelle)
-   const divIconTrash = document.createElement("div");
-  divIconTrash.classList.add("container-icons");
-  divIconTrash.classList.add("container-trash");
   
-  divIconsContainer.appendChild(divIconTrash);
-    //pour l'icon "poubelle"
-  const iconTrash = document.createElement("i");
-  iconTrash.classList.add("fa-solid");
-  iconTrash.classList.add("fa-xs");
-  iconTrash.classList.add("fa-trash-can");
-  divIconTrash.appendChild(iconTrash);
-    
-        
+  //un bouton "poubelle"
+  const btnDelete = document.createElement("button");
+  btnDelete.innerHTML =  `<i class="fa-solid fa-trash-can"></i>`;
+  btnDelete.classList.add("delete-button");
+  baliseFigureModale.appendChild(btnDelete);
+
       // un texte
   let textCardElementModale = document.createElement("figcaption");  
   textCardElementModale.innerText = "éditer";
   textCardElementModale.classList.add("text-miniature");
   baliseFigureModale.appendChild(textCardElementModale);
-  }
+
+  btnDelete.addEventListener("clic", () => {
+  supprimerProjet(work.id)
+  console.log(btnDelete)
+  
+  });
   
 }
+
 
 //**************************************************** */
 // fonction appel fetch "get" pour générer la galerie  */
@@ -101,7 +99,7 @@ async function genererGetWorks () {
           }
          
   if (!response.ok) {
-      throw new Error(`Erreur serveur`);
+      throw new Error("Erreur serveur");
   }
   
       works = await response.json()
@@ -117,6 +115,9 @@ async function genererGetWorks () {
   return works
 
 }
+
+
+
 
 //*********modale 2 ****************/
 /*  changement de modal1-->modal2  */                             
@@ -156,7 +157,7 @@ btnNavigationArriere.addEventListener("click", function (e) {
 
 //***********importer une image*************************** */
 /*                                                         */
-/*    importer un works depuis son ordinateur                                                     */
+/*    importer une image depuis son ordinateur                                                     */
 /********************************************************* */
 const imageInput = document.querySelector("#imageInput");
 const imageIconeImage = document.querySelector("#icone-image");
@@ -187,68 +188,41 @@ imageInput.addEventListener('change', function(event) {
             imageInput.style.display ="none";       // faire disparaitre l'imput pour une seule image à la fois
             btnValiderModal2.classList.add("active")    // rendre actif le bouton de validation du formulaire 
           }
-
-
-
-
         reader.readAsDataURL(selectedImage);
-          // Réinitialisation de la valeur de l'input file
+          // Réinitialisation de la valeur de l'input file au rechargement de la page
         imageInput.value = ""; // Cela permet à l'utilisateur de sélectionner une nouvelle image
         imageDisplay.innerHTML ="";
     }
 });
-//************************************************************************
-            //envoyer un nouveau "works" dans l'api via le formulaire
 
-//************************************************************************/
-const envoyerImageEtDonnees = async (imageUrl, titre, categorie) => {
 
-// Parcourri l'option de la catégorie qui a été selectionné par le formulaire déroulant:
-const selectElement = document.getElementById("categorychoix");
-const selectedOption = selectElement.options[selectElement.selectedIndex];
-const categoryId = selectedOption.value;
+//*********************************************************** */
+//        suppression d'un projet (works)                             
+//************************************************************ */
 
-console.log(categoryId)
+async function supprimerProjet(id) {
 
 
 
 
-  const formData = new FormData();
-  formData.append("image", imageUrl);
-  formData.append("title", titre);
-  formData.append("categoryId", categorie); // Assurez-vous que "categoryId" est correctement extrait du formulaire
 
   try {
-      const response = await fetch("http://localhost:5678/api/works", {
-          method: "POST", 
-          headers: { "Content-Type": "application/json" },
-          body: formData
+      const response = await fetch(`http://localhost:5678/api/works/${id}`, {
+          method: "DELETE",
+          headers: {
+              "Content-Type": "application/json",
+              "Authorization": "Bearer " + token
+          }
       });
-
-      if (!response.ok) {
-          throw new Error("Erreur lors de l\'envoi des données");
+      console.log(response)
+      if (response.ok) {
+          console.log("Projet supprimé avec succès.");
+      } else {
+          console.log("Erreur lors de la suppression.");
       }
-
-      const data = await response.json();
-      console.log("Réponse de l\'API:", data);
-      
-
   } catch (error) {
-      console.error('Erreur:', error);
-      // Gérer les erreurs en conséquence
-  }
-};
-
-// fonction d'envoi
-btnValiderModal2.addEventListener("click", async function(event) {
-  event.preventDefault();
-    //Point d'entrée au DOM des differents champs
-  const titre = document.querySelector("#title").value;
-  const categorie = document.querySelector("#categorychoix").value;
-   // envoyer la requete à la condition que l'image et les champs ne soit pas vide
-   if (imageDisplay.src && imageDisplay.style.display !== "none" && titre.trim() !== "" && categorie.trim() !== "") {
-    await envoyerImageEtDonnees(imageDisplay.src, titre, categorie);
-} else {
-    console.log("Veuillez remplir tous les champs du formulaire et sélectionner une image.");
+      console.error("Erreur:", error);
+      console.log("Une erreur s'est produite lors de la suppression.");
+  }}
 }
-});
+
